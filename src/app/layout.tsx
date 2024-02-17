ef006eb9a5afb985a555bacf623bce3acbddeedf
ilkinsufi <ilkinsufi@gmail.com>
2024-02-17 21:43:27 +0400

    First commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} dark bg-[#000000f6]`}>{children}</body>
     </html>
   );
 }
