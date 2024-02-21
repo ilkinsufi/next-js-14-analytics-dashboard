61a8cce4ef8a1937190f8af264052ffc261d1d3b
ilkinsufi <ilkinsufi@gmail.com>
2024-02-21 22:57:13 +0400

    Tremor add

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,8 +1,14 @@
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
-  const pageview = await analytics.retriveDays("pageview", 2);
+  const pageview = await analytics.retrieveDays("pageview", 2);
 
-  return <p>hello world</p>;
+  return (
+    <div className="min-h-screen w-full flex py-12 justify-center items-center">
+      <div className="relative w-full max-w-6xl mx-auto text-white">
+        <h1 className="text-4xl font-bold mb-4">Analytics</h1>
+      </div>
+    </div>
+  );
 };
 export default Page;
--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,5 +1,6 @@
 import { redis } from "@/lib/redis";
 import { getDate } from "@/utils";
+import { parse } from "date-fns";
 
 type AnalyticsArgs = {
   retention?: number;
@@ -31,6 +32,32 @@ export class Analytics {
     if (!opts?.persist) {
       await redis.expire(key, this.retention);
     }
+  }
+
+  async retrieveDays(namespace: string, nDays: number) {
+    type AnalyticsPromise = ReturnType<typeof analytics.retrieve>;
+    const promises: AnalyticsPromise[] = [];
+
+    for (let i = 0; i < nDays; i++) {
+      const formattedDate = getDate(i);
+      const primise = analytics.retrieve(namespace, formattedDate);
+      promises.push(primise);
+    }
+
+    const fetched = await Promise.all(promises);
+
+    const data = fetched.sort((a, b) => {
+      if (
+        parse(a.date, "dd/MM/yyyy", new Date()) >
+        parse(b.date, "dd/MM/yyyy", new Date())
+      ) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    return data;
   }
 
   async retrieve(namespace: string, date: string) {
