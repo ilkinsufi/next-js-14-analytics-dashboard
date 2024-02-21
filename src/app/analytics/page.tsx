2dd77c6a6fc653e579c58383401f50f93ddf60d6
ilkinsufi <ilkinsufi@gmail.com>
2024-02-21 22:42:57 +0400

    Analytics add

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,4 +1,8 @@
-const Page = () => {
+import { analytics } from "@/utils/analytics";
+
+const Page = async () => {
+  const pageview = await analytics.retriveDays("pageview", 2);
+
   return <p>hello world</p>;
 };
 export default Page;
